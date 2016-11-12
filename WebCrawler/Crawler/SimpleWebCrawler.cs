b45b3418879345318a7402f262e0535f53a25063
speane <avtolubitel225@gmail.com>
2016-11-12 23:20:48 +0300

    Created project for a library with Crawler

---
+++ WebCrawler/Crawler/SimpleWebCrawler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebCrawler.Result;
+
+namespace WebCrawler.Crawler
+{
+    public class SimpleWebCrawler : ISimpleWebCrawler
+    {
+        public async Task<CrawlResult> PerformCrawlingAsync(string[] rootUrls)
+        {
+            return new CrawlResult();
+        }
+    }
+}

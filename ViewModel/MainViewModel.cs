6b16fadcb77a9eaf44afb9cb365acef0816687ff
speane <avtolubitel225@gmail.com>
2016-11-12 20:47:27 +0300

    Implemented IIncrementable and INotifyPropertyChanged by MainView model

---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using MPP.WebCrawler.Annotations;
+
+namespace MPP.WebCrawler.ViewModel
+{
+    public class MainViewModel : IIncrementable, INotifyPropertyChanged
+    {
+        private int _count;
+
+        public int Count {
+            get { return _count; }
+            set
+            {
+                _count = value;
+                RaiseOnPropertyChanged(nameof(Count));
+            }
+        }
+
+        public void Increment()
+        {
+            Count++;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void RaiseOnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
